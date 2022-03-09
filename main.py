4e9db4be57a70293ee85269199c644304a115e68
dimabaydukov <18052002@bk.ru>
2022-03-09 22:13:36 +0300

    Подключил pygame
    Подключил PyGame и создал окно с игровым циклом и ограничением в 60 кадров

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Начался проект")
+import pygame
+
+pygame.init()
+
+pygame.display.set_mode((600, 400), pygame.RESIZABLE)
+pygame.display.set_caption("Замечательная игра")
+
+clock = pygame.time.Clock()
+FPS = 60
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    clock.tick(FPS)
+    
