3053e650578a68c7583bcaf60d321db514029ad9
dimabaydukov <18052002@bk.ru>
2022-03-21 17:44:32 +0300

    Рамка и цвета
    Нарисовал рамку и добавил цвета

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import pygame
 
 pygame.init()
 
-pygame.display.set_mode((600, 400), pygame.RESIZABLE)
+sc = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
 pygame.display.set_caption("Замечательная игра")
 
 clock = pygame.time.Clock()
 FPS = 60
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+pygame.draw.rect(sc, WHITE, (10, 10, 580, 380), 3)
+pygame.display.update()
 
 while True:
     for event in pygame.event.get():
@@ -14,4 +23,3 @@ while True:
             exit()
 
     clock.tick(FPS)
-    
