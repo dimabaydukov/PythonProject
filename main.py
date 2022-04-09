eb14e7d5d0b50ce6861558d64fe301440f5b266e
dimabaydukov <18052002@bk.ru>
2022-04-09 19:10:03 +0300

    Создание заголовка

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import pygame
 
 pygame.init()
 
-sc = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
-pygame.display.set_caption("Замечательная игра")
+WIDTH = 600
+HEIGHT = 400
+
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Learning English")
 
 clock = pygame.time.Clock()
 FPS = 60
@@ -13,6 +16,11 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+
+font = pygame.font.SysFont('Arial', 22)
+sc_title = font.render('Hello! Nice to meet you here', True, WHITE)
+pos_title = sc_title.get_rect(center=(WIDTH//2, 50))
+sc.blit(sc_title, pos_title)
 
 pygame.draw.rect(sc, WHITE, (10, 10, 580, 380), 3)
 pygame.display.update()
