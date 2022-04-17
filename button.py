c48196805d3b2c95533c10d0f63e78ba8f7de3d7
dimabaydukov <18052002@bk.ru>
2022-04-18 01:21:59 +0300

    Главное меню

---
+++ button.py
@@ -0,0 +1,54 @@
+import pygame
+
+
+# класс для создания кнопок
+class Button:
+    def __init__(self, text, width, height, pos, top_color, bottom_color, font, font_color, hover_color, elevation):
+        # атрибуты кнопки
+        self.elevation = elevation
+        self.previous_elevation = elevation
+        self.original_y_position = pos[1]
+
+        self.previous_color = top_color
+        self.hover_color = hover_color
+        self.pressed = False
+
+        # верхняя часть кнопки
+        self.top_rectangle = pygame.Rect(pos, (width, height))
+        self.top_color = top_color
+
+        # нижняя часть кнопки
+        self.bottom_rectangle = pygame.Rect(pos, (width, elevation))
+        self.bottom_color = bottom_color
+
+        # текст
+        self.text_surface = font.render(text, True, font_color)
+        self.text_rectangle = self.text_surface.get_rect(center=self.top_rectangle.center)
+
+    # отрисовка кнопки
+    def draw(self, surface):
+        self.top_rectangle.y = self.original_y_position - self.previous_elevation
+        self.text_rectangle.center = self.top_rectangle.center
+
+        self.bottom_rectangle.midtop = self.top_rectangle.midtop
+        self.bottom_rectangle.height = self.top_rectangle.height + self.previous_elevation
+
+        pygame.draw.rect(surface, self.bottom_color, self.bottom_rectangle, border_radius=15)
+        pygame.draw.rect(surface, self.top_color, self.top_rectangle, border_radius=15)
+        surface.blit(self.text_surface, self.text_rectangle)
+
+    # проверка на нажатие мыши
+    def check_click(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.top_rectangle.collidepoint(mouse_pos):
+            self.top_color = self.hover_color
+            if pygame.mouse.get_pressed()[0]:
+                self.previous_elevation = 0
+                self.pressed = True
+            else:
+                self.previous_elevation = self.elevation
+                if self.pressed:
+                    self.pressed = False
+        else:
+            self.previous_elevation = self.elevation
+            self.top_color = self.previous_color
--- main.py
+++ main.py
@@ -1,33 +1,74 @@
-import pygame
+from button import Button
 
+# инициализация библиотеки PyGame и звуковых эффектов в ней
+import pygame
+pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.init()
 
-WIDTH = 600
-HEIGHT = 400
+# параметры окна
+WIDTH = 800
+HEIGHT = 600
 
-sc = pygame.display.set_mode((WIDTH, HEIGHT))
+# создание окна, изменение названия и иконки приложения
+main_surface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Learning English")
+pygame.display.set_icon(pygame.image.load("Images/icon.bmp"))
 
+# количество кадров в секунду
 clock = pygame.time.Clock()
 FPS = 60
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+# инициализация цветов
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+BLUE = (31, 117, 254)
+DARK_BLUE = (4, 41, 131)
 
-font = pygame.font.SysFont('Arial', 22)
-sc_title = font.render('Hello! Nice to meet you here', True, WHITE)
-pos_title = sc_title.get_rect(center=(WIDTH//2, 50))
-sc.blit(sc_title, pos_title)
+# задний фон
+background_surface = pygame.image.load("Images/background.png").convert()
+main_surface.blit(background_surface, (0, 0))
 
-pygame.draw.rect(sc, WHITE, (10, 10, 580, 380), 3)
+# музыка главного меню
+pygame.mixer.music.load("Music/background.mp3")
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(0.15)
+
+# заголовок
+title_font = pygame.font.Font("Fonts/main.ttf", 28)
+title_surface = title_font.render("Hello! Welcome to this wonderful game", True, BLACK)
+title_position = title_surface.get_rect(center=(WIDTH//2, 75))
+main_surface.blit(title_surface, title_position)
+
+# подзаголовок
+small_title_font = pygame.font.Font("Fonts/main.ttf", 22)
+small_title_surface1 = small_title_font.render("Here you can improve your English skills", True, BLACK)
+small_title_position1 = small_title_surface1.get_rect(center=(WIDTH//2, 125))
+small_title_surface2 = small_title_font.render("by completing various interesting levels", True, BLACK)
+small_title_position2 = small_title_surface2.get_rect(center=(WIDTH//2, 145))
+main_surface.blit(small_title_surface1, small_title_position1)
+main_surface.blit(small_title_surface2, small_title_position2)
+
+# шрифт для кнопок в главном меню
+button_font_main = pygame.font.Font("Fonts/main.ttf", 24)
+
+# создание кнопок
+play_button = Button("Play", 200, 50, (WIDTH//2 - 100, HEIGHT//2 - 25), BLUE, DARK_BLUE, button_font_main,
+                     WHITE, DARK_BLUE, 5)
+play_button.draw(main_surface)
+exit_button = Button("Exit", 200, 50, (WIDTH//2 - 100, HEIGHT//2 + 50), BLUE, DARK_BLUE, button_font_main,
+                     WHITE, DARK_BLUE, 5)
+exit_button.draw(main_surface)
+
 pygame.display.update()
 
+# игровой круг
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-
+    play_button.check_click()
+    play_button.draw(main_surface)
+    exit_button.check_click()
+    exit_button.draw(main_surface)
+    pygame.display.update()
     clock.tick(FPS)
