157c4c800b2b00c60909bb8a43b63220e6d97bb2
dimabaydukov <18052002@bk.ru>
2022-04-23 02:57:15 +0300

    Картинки выбора уровня

--- main.py
+++ main.py
@@ -101,24 +101,56 @@ def main_menu():
 def levels():
     running = True
 
+    main_surface.fill(WHITE)
+    main_surface.blit(background_surface, (0, 0))
+
+    level_text_surface = small_title_font.render("Choose your level", True, BLACK)
+    level_text_position = level_text_surface.get_rect(center=(WIDTH // 2, 75))
+    main_surface.blit(level_text_surface, level_text_position)
+
+    image_font = pygame.font.Font("Fonts/main.ttf", 14)
+
+    animals = pygame.image.load("Images/animals.jpg").convert()
+    animals_text = image_font.render("Animals", True, BLACK)
+    main_surface.blit(animals_text, (140, 100))
+    animals_rect = animals.get_rect(topleft=(140, 120))
+    animals_hover = pygame.image.load("Images/animals_hover.jpg").convert()
+
+    food = pygame.image.load("Images/food.jpg").convert()
+    food_text = image_font.render("Food", True, BLACK)
+    main_surface.blit(food_text, (140, 300))
+    food_rect = food.get_rect(topleft=(140, 320))
+    food_hover = pygame.image.load("Images/food_hover.jpg").convert()
+
+    sights = pygame.image.load("Images/sights.jpg").convert()
+    sights_text = image_font.render("Sights", True, BLACK)
+    main_surface.blit(sights_text, (WIDTH - 320, 100))
+    sights_rect = sights.get_rect(topleft=(WIDTH - 320, 120))
+    sights_hover = pygame.image.load("Images/sights_hover.jpg").convert()
+
+    it = pygame.image.load("Images/it.jpg").convert()
+    it_text = image_font.render("IT", True, BLACK)
+    main_surface.blit(it_text, (WIDTH - 320, 300))
+    it_rect = it.get_rect(topleft=(WIDTH - 320, 320))
+    it_hover = pygame.image.load("Images/it_hover.jpg").convert()
+
+    back_arrow = pygame.image.load("Images/back.png").convert_alpha()
+    arrow_position = back_arrow.get_rect(center=(WIDTH // 2, HEIGHT - 100))
+    back_arrow_hover = pygame.image.load("Images/back_hover.png").convert_alpha()
+
     while running:
+        main_surface.blit(animals, (140, 120))
+        main_surface.blit(food, (140, 320))
+        main_surface.blit(sights, (WIDTH - 320, 120))
+        main_surface.blit(it, (WIDTH - 320, 320))
+
+        main_surface.blit(back_arrow, arrow_position)
+
         mouse_pos = pygame.mouse.get_pos()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
-
-        main_surface.fill(WHITE)
-        main_surface.blit(background_surface, (0, 0))
-
-        level_text_surface = small_title_font.render("Choose your level", True, BLACK)
-        level_text_position = level_text_surface.get_rect(center=(WIDTH // 2, 75))
-        main_surface.blit(level_text_surface, level_text_position)
-
-        back_arrow = pygame.image.load("Images/back.png").convert_alpha()
-        arrow_position = back_arrow.get_rect(center=(WIDTH // 2, HEIGHT - 100))
-        main_surface.blit(back_arrow, arrow_position)
-        back_arrow_hover = pygame.image.load("Images/back_hover.png").convert_alpha()
 
         if arrow_position.collidepoint(mouse_pos):
             main_surface.blit(back_arrow_hover, arrow_position)
@@ -126,9 +158,49 @@ def levels():
                 click.play()
                 main_menu()
                 running = False
+        elif animals_rect.collidepoint(mouse_pos):
+            main_surface.blit(animals_hover, (140, 120))
+            if pygame.mouse.get_pressed()[0]:
+                click.play()
+                animals_def()
+                running = False
+        elif food_rect.collidepoint(mouse_pos):
+            main_surface.blit(food_hover, (140, 320))
+            if pygame.mouse.get_pressed()[0]:
+                click.play()
+                food_def()
+                running = False
+        elif sights_rect.collidepoint(mouse_pos):
+            main_surface.blit(sights_hover, (WIDTH - 320, 120))
+            if pygame.mouse.get_pressed()[0]:
+                click.play()
+                sights_def()
+                running = False
+        elif it_rect.collidepoint(mouse_pos):
+            main_surface.blit(it_hover, (WIDTH - 320, 320))
+            if pygame.mouse.get_pressed()[0]:
+                click.play()
+                it_def()
+                running = False
 
         pygame.display.update()
         clock.tick(FPS)
+
+
+def animals_def():
+    pass
+
+
+def food_def():
+    pass
+
+
+def sights_def():
+    pass
+
+
+def it_def():
+    pass
 
 
 main_menu()
